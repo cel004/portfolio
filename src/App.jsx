f57bcd9dd41056db097c44059a3c46166e6171ff
cel004 <ngcelinaa@gmail.com>
2025-02-10 23:46:47 -0500

    Import fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
+      <p>Urbanist</p>
+      <p style={{ fontFamily: 'IntroScript' }}>IntroScript</p>
+      <p style={{ fontFamily: 'SaoTorpes' }}>Sao Torpes</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
