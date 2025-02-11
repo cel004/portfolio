a13deb8b6c46cf9b1726341464dc9c74d64fbec9
cel004 <ngcelinaa@gmail.com>
2025-02-11 15:40:48 -0500

    Add drawer container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import styled from "styled-components";
 import drawerFront from "./assets/drawer_front.svg";
+import drawerContainer from "./assets/drawer_container.svg";
 
-const StyledImage = styled.img`
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const DrawerImage = styled.img`
+  display: flex;
+  justify-content: center;
   width: 95vw; 
   height: auto;
-  margin-top: 30rem;
+  z-index: -100;
+`;
+const ContainerImage = styled.img`
+  display: flex;
+  justify-content: center;
+  width: 90vw; 
+  height: auto;
+  z-index: -100;
 `;
 
 const App = () => {
   return (
     <>
-      <StyledImage src={drawerFront} alt="Front of file drawer" />
+    <Container>
+      <ContainerImage src={drawerContainer} alt="Container of file drawer" />
+      <DrawerImage src={drawerFront} alt="Front of file drawer" />
+    </Container>
     </>
   );
 };
